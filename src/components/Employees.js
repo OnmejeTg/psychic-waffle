89c42b9acbfffee482e060b577f2965d7d939073
tgod <tonmeje@gmail.com>
2022-11-04 04:39:30 +0100

    mobile styling

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -67,8 +67,8 @@ function Employees() {
   }
 
   return (
-    <div className="App bg-gray-300 min-h-screen">
-      <div className="flex flex-wrap justify-center my-2">
+    <div className="App">
+      <div className="flex flex-wrap justify-center">
         {employees.map((employee) => {
           const editEmployee = (
             <EditEmployee
