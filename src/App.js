dfef8f0e11c1ec4bcda71c367079a715cfd93a9b
tgod <tonmeje@gmail.com>
2022-11-04 03:51:14 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import EditEmployee from "./components/EditEmployee";
 import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import Employees from "./components/Employees";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  
   return (
     <Header>
-       <Employees/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/e" element={<Employees />} />
+        </Routes>
+      </BrowserRouter>
     </Header>
-   
-  )
-    
+  );
 }
 
 export default App;
