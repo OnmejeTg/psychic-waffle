ceef3fd547103907e7bf18a8e044782095ac4bbb
tgod <tonmeje@gmail.com>
2022-11-05 22:16:09 +0100

    Dependency array

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -3,12 +3,14 @@ import { useState, useEffect } from "react"
 const Dictionary = () => {
     const [word, setWord] = useState()
 
-    useEffect(()=>{})
+    useEffect(()=>{
+        console.log('Stated Updated', word)
+    }, [])
   return (
     <div>
       <input type="text" onChange={(e)=>{
         setWord(e.target.value)
-        console.log('Stated Updated', word)
+        
       }} />
       <h1>{word}</h1>
     </div>
