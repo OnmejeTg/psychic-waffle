8d3a226d22d5eb5e2436dc03b62850b82dfff2a2
tgod <tonmeje@gmail.com>
2022-11-07 12:22:28 +0100

    multiple useEffect

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,18 +1,29 @@
 import { useState, useEffect } from "react"
 
 const Dictionary = () => {
-    const [word, setWord] = useState()
+    const [word, setWord] = useState('')
+    const [word2, setWord2] = useState('')
 
     useEffect(()=>{
-        console.log('Stated Updated', word)
-    }, [])
+        console.log('Stated Updated ' +  word )
+    }, [word])
+
+    useEffect(()=>{
+        console.log('Stated Updated '+ word2)
+    }, [word2])
+    
   return (
     <div>
       <input type="text" onChange={(e)=>{
         setWord(e.target.value)
         
       }} />
-      <h1>{word}</h1>
+      <h1>Get defination for {word}</h1>
+      <input type="text" onChange={(e)=>{
+        setWord2(e.target.value)
+        
+      }} />
+      <h1>Get defination for {word2}</h1>
     </div>
   )
 }
