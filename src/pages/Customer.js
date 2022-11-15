4f8a26cd82ac46a77903118b67c1fe7ae636cd1d
tgod <tonmeje@gmail.com>
2022-11-15 19:57:23 +0100

    Return 404 from Backend API (Django)

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,13 +1,18 @@
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { baseUrl } from "../shared";
 
 const Customer = () => {
   const { id } = useParams();
   const [customer, setCustomer] = useState();
+  const [notFound, setNotFound] = useState();
   useEffect(() => {
-    const url = "http://127.0.0.1:8000/api/customer/" + id;
+    const url = baseUrl + "api/customer/" + id;
     fetch(url)
       .then((response) => {
+        if (response.status === 404) {
+          setNotFound(true);
+        }
         return response.json();
       })
       .then((data) => {
@@ -17,14 +22,15 @@ const Customer = () => {
 
   return (
     <div>
-      {customer ? 
+      {notFound ? <p>No customer with id {id}</p> : null}
+      {customer ? (
         <div>
           <p>{customer.id}</p>
           <p>{customer.name}</p>
           <p>{customer.industry}</p>
         </div>
-       : null}
-       <Link to='/customers'>Go back</Link>
+      ) : null}
+      <Link to="/customers">Go back</Link>
     </div>
   );
 };
--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { baseUrl } from "../shared";
+
 const Customers = () => {
   const [customers, setCustomers] = useState();
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/customers")
+    const url = baseUrl + "api/customers";
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setCustomers(data.customers);
@@ -12,11 +15,15 @@ const Customers = () => {
   return (
     <div>
       <h2>Hera are our customers</h2>
-      {customers? customers.map((customer)=>{
-        return(
-          <p><Link to={'/customers/'+customer.id}>{customer.name}</Link></p>
-        )
-      }):null}
+      {customers
+        ? customers.map((customer) => {
+            return (
+              <p>
+                <Link to={"/customers/" + customer.id}>{customer.name}</Link>
+              </p>
+            );
+          })
+        : null}
     </div>
   );
 };
