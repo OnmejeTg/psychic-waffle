29730deafa0b78ab477be1d26d2963699d836285
tgod <tonmeje@gmail.com>
2022-11-30 20:06:26 +0100

    added update customer functionality

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -6,7 +6,13 @@ const Customer = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [customer, setCustomer] = useState();
+  const [tempCustomer, setTempCustomer] = useState();
   const [notFound, setNotFound] = useState();
+  const [changed, setchanged] = useState(false);
+
+  useEffect(() => {
+    // console.log(changed);
+  });
 
   useEffect(() => {
     const url = baseUrl + "api/customer/" + id;
@@ -19,30 +25,82 @@ const Customer = () => {
       })
       .then((data) => {
         setCustomer(data.customer);
+        setTempCustomer(data.customer);
       });
   }, []);
+
+  function updateCustomer() {
+    const url = baseUrl + "api/customer/" + id;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(tempCustomer),
+    })
+      .then((response) => {
+        return response.json;
+      })
+      .then((data) => {
+        setchanged(false)
+        console.log(data);
+      })
+      .catch();
+  }
 
   return (
     <div>
       {notFound ? <p>No customer with id {id}</p> : null}
       {customer ? (
         <div>
-          <p>{customer.id}</p>
-          <p>{customer.name}</p>
-          <p>{customer.industry}</p>
+          <p className="m-2 block ">ID: {tempCustomer.id}</p>
+          <input
+            className="m-2 block px-2"
+            type="text"
+            value={tempCustomer.name}
+            onChange={(e) => {
+              setchanged(true);
+              setTempCustomer({ ...tempCustomer, name: e.target.value });
+            }}
+          />
+          <input
+            className="m-2 block px-2"
+            type="text"
+            value={tempCustomer.industry}
+            onChange={(e) => {
+              setchanged(true);
+              setTempCustomer({ ...tempCustomer, industry: e.target.value });
+            }}
+          />
+          {changed ? (
+            <>
+              <button
+                onClick={() => {
+                  setTempCustomer({ ...customer });
+                  setchanged(false);
+                }}
+              >
+                Cancel
+              </button>{" "}
+              <button onClick={updateCustomer}>Save</button>
+            </>
+          ) : null}
         </div>
       ) : null}
       <button
         onClick={(e) => {
           const url = baseUrl + "api/customer/" + id;
-          fetch(url, { method: "DELETE", headers:{
-            'Content-type':'application/json'
-          } })
-            .then((response) => { 
+          fetch(url, {
+            method: "DELETE",
+            headers: {
+              "Content-type": "application/json",
+            },
+          })
+            .then((response) => {
               if (!response.ok) {
                 throw new Error("Something went wrong");
               }
-              navigate('/customers')
+              navigate("/customers");
             })
             .catch((e) => {
               console.log(e);
